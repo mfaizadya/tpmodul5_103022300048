842a8b18b6ec4b178a362b1df31041fb2b97879d
mfaizadya <m.faiz.adya@gmail.com>
2025-03-16 13:32:01 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class DataGeneric<T>
+{
+    private T data;
+    
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpang adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022300048");
+        data.PrintData();
+    }
+}
